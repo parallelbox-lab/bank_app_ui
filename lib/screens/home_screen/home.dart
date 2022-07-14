31ca278a260c27363b11390d075e20ed0341bf13
Owoeye precious <72979479+parallelbox-lab@users.noreply.github.com>
2022-07-14 21:54:46 +0100

    commit

--- lib/screens/home_screen/home.dart
+++ lib/screens/home_screen/home.dart
@@ -8,17 +8,28 @@ class Home extends StatelessWidget {
     return Scaffold(
       body:SafeArea(
         child: ListView(children: [
-          Padding(
-            padding: const EdgeInsets.all(15.0),
-            child: Row(
-              children: [
-                
-                
-              ],
-            ),
-          )
+          _buildAppBar()
         ],),
       )
     );
+  }
+
+   _buildAppBar() {
+    return Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Row(
+            children: [
+              Image.asset("assets/circleProfile.png"),
+              const SizedBox(width:5),
+              const Text('Hi, Precious'),
+              const Spacer(),
+              Image.asset('assets/search.png'),
+              const Spacer(),
+              Image.asset('assets/search.png'),
+
+              
+            ],
+          ),
+        );
   }
 }
