b60b2a21ee970ac3d203ec905399b9b8c2355633
Owoeye precious <72979479+parallelbox-lab@users.noreply.github.com>
2022-07-15 00:10:59 +0100

    commit

--- lib/screens/home_screen/home.dart
+++ lib/screens/home_screen/home.dart
@@ -26,105 +26,124 @@ class Home extends StatelessWidget {
           _buildAppBar(greetings()),
           _walletBalance(),
           const SizedBox(height:25),
-          Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 15),
-            child: GridView.count(
-              padding:const EdgeInsets.all(1),
-              shrinkWrap: true,
-              crossAxisCount: 3,
-              crossAxisSpacing: 0.0,
-              children: [
-             Container(
-              decoration:const BoxDecoration(
-                color: Color.fromARGB(26, 135, 239, 135),
-                borderRadius: BorderRadius.all(Radius.circular(8))
-              ),
-              padding:const EdgeInsets.all(0),
-              margin:const EdgeInsets.all(8.0),              
-              child:Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children:  [
-                const Icon(Icons.wallet_giftcard,color:Color(0xff90ee90)),
-                const SizedBox(height: 7,),
-                CustomText(text: "Fund Wallet",color: Color(0xff90ee90),weight: FontWeight.bold,)
-               ],)),
-               Container(
-              padding:const EdgeInsets.all(0),
-              margin:const EdgeInsets.all(8.0),
-              decoration:const BoxDecoration(
-                color: Color.fromARGB(26, 83, 44, 19),
-                borderRadius: BorderRadius.all(Radius.circular(8))
-              ),
-                child:Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                const Icon(Icons.card_membership_outlined,color: Color(0xff5C3317)),
-                const SizedBox(height: 7,),
-                CustomText(text:"Send Funds",color: Color(0xff5C3317),weight: FontWeight.bold,)
-               ],)),
-               Container(
-              padding:const EdgeInsets.all(0),
-              margin:const EdgeInsets.all(8.0),
-              decoration:const BoxDecoration(
-                color: Color(0x1AC04000),
-                borderRadius: BorderRadius.all(Radius.circular(8))
-              ),
-              child:Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                const Icon(Icons.wallet_giftcard),
-                const SizedBox(height: 7,),
-                CustomText(text: "Cash Pin",color:Color(0xffC04000),weight: FontWeight.bold,)
-               ],)),
-              Container(
-              padding:const EdgeInsets.all(0),
-              margin:const EdgeInsets.all(8.0),
-              decoration:const BoxDecoration(
-                color: Color(0x1AFF0000),
-                borderRadius: BorderRadius.all(Radius.circular(8))
-              ),
-              child:Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                const Icon(Icons.wallet_giftcard),
-                const SizedBox(height: 7,),
-                CustomText(text: "Airtime",color:Color(0xffFF0000),weight: FontWeight.bold,)
-               ],)),
-               Container(
-              padding:const EdgeInsets.all(0),
-              margin:const EdgeInsets.all(8.0),
-              decoration:const BoxDecoration(
-                color: Color(0x1A6A0DAD),
-                borderRadius: BorderRadius.all(Radius.circular(8))
-              ),
-              child:Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                const Icon(Icons.wallet_giftcard,),
-                const SizedBox(height: 7,),
-                CustomText(text: "Pay Bills",color:Color(0xff6A0DAD),weight: FontWeight.bold,)
-               ],)),
-             Container(
-              decoration:const BoxDecoration(
-                color: Color(0x1A7D0552),
-                borderRadius: BorderRadius.all(Radius.circular(8))
-              ),
-              padding:const EdgeInsets.all(0),
-              margin:const EdgeInsets.all(8.0),
-              child:Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                const Icon(Icons.wallet_giftcard),
-                const SizedBox(height: 7,),
-                CustomText(text: "View All",color:Color(0xff7D0552),weight: FontWeight.bold,)
-               ],))
-              ],
+          _buildAction(),
+          const SizedBox(height: 25,),
+           Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+       const Text('Transactions',
+            style:
+                TextStyle(fontSize: 15, fontWeight: FontWeight.w700)),
+        GestureDetector(
+          onTap: (){},
+          child:const Text('See all',
+          style:
+                TextStyle(fontSize: 15, fontWeight: FontWeight.w700)
             ),
-          )
-
+        ),
+      ],
+    )
         ],),
       )
     );
+  }
+
+  Padding _buildAction() {
+    return Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15),
+          child: GridView.count(
+            padding:const EdgeInsets.all(1),
+            shrinkWrap: true,
+            crossAxisCount: 3,
+            crossAxisSpacing: 0.0,
+            children: [
+           Container(
+            decoration:const BoxDecoration(
+              color: Color.fromARGB(26, 135, 239, 135),
+              borderRadius: BorderRadius.all(Radius.circular(8))
+            ),
+            padding:const EdgeInsets.all(0),
+            margin:const EdgeInsets.all(8.0),              
+            child:Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children:  [
+              const Icon(Icons.wallet_giftcard,color:Color(0xff90ee90)),
+              const SizedBox(height: 7,),
+              CustomText(text: "Fund Wallet",color: Color(0xff90ee90),weight: FontWeight.bold,)
+             ],)),
+             Container(
+            padding:const EdgeInsets.all(0),
+            margin:const EdgeInsets.all(8.0),
+            decoration:const BoxDecoration(
+              color: Color.fromARGB(26, 83, 44, 19),
+              borderRadius: BorderRadius.all(Radius.circular(8))
+            ),
+              child:Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+              const Icon(Icons.card_membership_outlined,color: Color(0xff5C3317)),
+              const SizedBox(height: 7,),
+              CustomText(text:"Send Funds",color: Color(0xff5C3317),weight: FontWeight.bold,)
+             ],)),
+             Container(
+            padding:const EdgeInsets.all(0),
+            margin:const EdgeInsets.all(8.0),
+            decoration:const BoxDecoration(
+              color: Color(0x1AC04000),
+              borderRadius: BorderRadius.all(Radius.circular(8))
+            ),
+            child:Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+              const Icon(Icons.wallet_giftcard),
+              const SizedBox(height: 7,),
+              CustomText(text: "Cash Pin",color:Color(0xffC04000),weight: FontWeight.bold,)
+             ],)),
+            Container(
+            padding:const EdgeInsets.all(0),
+            margin:const EdgeInsets.all(8.0),
+            decoration:const BoxDecoration(
+              color: Color(0x1AFF0000),
+              borderRadius: BorderRadius.all(Radius.circular(8))
+            ),
+            child:Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+              const Icon(Icons.wallet_giftcard),
+              const SizedBox(height: 7,),
+              CustomText(text: "Airtime",color:Color(0xffFF0000),weight: FontWeight.bold,)
+             ],)),
+             Container(
+            padding:const EdgeInsets.all(0),
+            margin:const EdgeInsets.all(8.0),
+            decoration:const BoxDecoration(
+              color: Color(0x1A6A0DAD),
+              borderRadius: BorderRadius.all(Radius.circular(8))
+            ),
+            child:Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+              const Icon(Icons.wallet_giftcard,),
+              const SizedBox(height: 7,),
+              CustomText(text: "Pay Bills",color:Color(0xff6A0DAD),weight: FontWeight.bold,)
+             ],)),
+           Container(
+            decoration:const BoxDecoration(
+              color: Color(0x1A7D0552),
+              borderRadius: BorderRadius.all(Radius.circular(8))
+            ),
+            padding:const EdgeInsets.all(0),
+            margin:const EdgeInsets.all(8.0),
+            child:Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+              const Icon(Icons.wallet_giftcard),
+              const SizedBox(height: 7,),
+              CustomText(text: "View All",color:Color(0xff7D0552),weight: FontWeight.bold,)
+             ],))
+            ],
+          ),
+        );
   }
 
    _walletBalance() {
