65543e4897198b93765701ff0af9a76f0efc2b58
Owoeye precious <72979479+parallelbox-lab@users.noreply.github.com>
2022-07-14 22:49:11 +0100

    c

--- lib/screens/home_screen/home.dart
+++ lib/screens/home_screen/home.dart
@@ -10,49 +10,53 @@ class Home extends StatelessWidget {
       body:SafeArea(
         child: ListView(children: [
           _buildAppBar(),
-          Container(
-            padding: const EdgeInsets.symmetric(vertical:9.0,horizontal: 15),
-            margin:const EdgeInsets.symmetric(horizontal: 15,vertical:5.0) ,
-            decoration:const BoxDecoration(
-              color:Colors.deepPurple,
-              borderRadius: BorderRadius.all(Radius.circular(8))
-            ),
-            child:Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(children: [
-                  Image.asset("assets/nigeria_logo.jpg",width:25),
-                  const SizedBox(width:10),
-                  const CustomText(text: "Wallet Balance",color: Colors.white,),
-                 const Spacer(),
-                  TextButton(onPressed:(){},
-                  child:Row(
-                    children: const [
-                    Icon(Icons.refresh),
-                    SizedBox(width:5),
-                    CustomText(text: 'Refresh Balance',color: Colors.white,),
-                    ],
-                  ),
-                  style: TextButton.styleFrom(
-                padding:const EdgeInsets.all(10),
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(4.0)),
-              primary: Colors.white,
-              backgroundColor: Colors.purpleAccent,
-            ),)
-                ],),
-               const SizedBox(height:25),
-               Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                 children: [
-                  const CustomText(text: "N52,0000",color: Colors.white,weight: FontWeight.bold,),
-                   IconButton(onPressed: (){}, icon:const Icon(Icons.remove_red_eye,color:Colors.white))
-                 ],
-               )
-            ],)
-          )
+          _walletBalance()
         ],),
       )
     );
+  }
+
+  Container _walletBalance() {
+    return Container(
+          padding: const EdgeInsets.symmetric(vertical:9.0,horizontal: 15),
+          margin:const EdgeInsets.symmetric(horizontal: 15,vertical:5.0) ,
+          decoration:const BoxDecoration(
+            color:Colors.deepPurple,
+            borderRadius: BorderRadius.all(Radius.circular(8))
+          ),
+          child:Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(children: [
+                Image.asset("assets/nigeria_logo.jpg",width:25),
+                const SizedBox(width:10),
+                const CustomText(text: "Wallet Balance",color: Colors.white,),
+               const Spacer(),
+                TextButton(onPressed:(){},
+                child:Row(
+                  children: const [
+                  Icon(Icons.refresh),
+                  SizedBox(width:5),
+                  CustomText(text: 'Refresh Balance',color: Colors.white,),
+                  ],
+                ),
+                style: TextButton.styleFrom(
+              padding:const EdgeInsets.all(10),
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(4.0)),
+            primary: Colors.white,
+            backgroundColor: Colors.purpleAccent,
+          ),)
+              ],),
+             const SizedBox(height:25),
+             Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+               children: [
+                const CustomText(text: "N52,0000",color: Colors.white,weight: FontWeight.bold,),
+                IconButton(onPressed: (){}, icon:const Icon(Icons.remove_red_eye,color:Colors.white))
+               ],
+             )
+          ],)
+        );
   }
 
    _buildAppBar() {
