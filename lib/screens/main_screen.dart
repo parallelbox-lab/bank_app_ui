6f0afc7f8b5db4758b985298a810366429e5f516
Owoeye precious <72979479+parallelbox-lab@users.noreply.github.com>
2022-07-15 02:34:21 +0100

    commit

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,80 +1,94 @@
 import 'package:bank_app_ui/screens/home_screen/home.dart';
-import 'package:bank_app_ui/widgets/custom_text.dart';
 import 'package:flutter/material.dart';
-// import 'package:flutter_travel_concept/widgets/icon_badge.dart';
+
 
 class MainScreen extends StatefulWidget {
- const MainScreen({ Key? key }) : super(key: key);
+  const MainScreen({Key? key}) : super(key: key);
+  static String routeName = "/page-view";
 
   @override
-  _MainScreenState createState() => _MainScreenState();
+  State<MainScreen> createState() => _MainScreenState();
 }
 
 class _MainScreenState extends State<MainScreen> {
-  PageController? _pageController;
-  int _page = 0;
+  final List<Widget> _pages = <Widget>[
+  const Home(),
+  ];
+  final PageController _pageController = PageController();
+  int _selectedIndex = 0;
+  void _onPageChanged(int index) {
+    _selectedIndex = index;
+  }
+
+  void _itemTapped(int _selectedIndex) {
+    setState(
+      () {
+        _pageController.jumpToPage(_selectedIndex);
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: PageView(
-        physics:const NeverScrollableScrollPhysics(),
         controller: _pageController,
-        onPageChanged: onPageChanged,
-        children: List.generate(4, (index) => Home()),
+        children: _pages,
+        onPageChanged: _onPageChanged,
+        physics: const NeverScrollableScrollPhysics(),
       ),
-      bottomNavigationBar: BottomAppBar(
-        child: Row(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-           const SizedBox(width: 7.0),
-            barIcon(icon: Icons.home, text:"Home", page: 0),
-            barIcon(icon: Icons.pin, text:"CashPins", page: 1),
-            barIcon(icon: Icons.card_giftcard_outlined,text:"Cards", page: 2, badge: true),
-            barIcon(icon: Icons.menu,text:"Menu", page: 3),
-           const  SizedBox(width: 7.0),
-          ],
-        ),
-        color: Theme.of(context).primaryColor,
+      bottomNavigationBar: SizedBox(
+        height: 70,
+        child: BottomNavigationBar(
+            currentIndex: _selectedIndex,
+            // selectedItemColor: Colors.black,
+            unselectedItemColor: Colors.grey,
+            showUnselectedLabels: true,
+            elevation: 3,
+            selectedLabelStyle:const TextStyle(
+                fontFamily: 'Core Pro',
+                fontSize: 11.0,
+                fontWeight: FontWeight.w600,
+                fontStyle: FontStyle.normal),
+            unselectedLabelStyle:const TextStyle(
+                fontFamily: 'Core Pro',
+                fontSize: 11.0,
+                fontWeight: FontWeight.w600,
+                fontStyle: FontStyle.normal),
+            onTap: _itemTapped,
+            items: [
+              BottomNavigationBarItem(
+                icon: Container(
+                    margin: const EdgeInsets.only(bottom: 10),
+                    child: Image.asset(
+                      'assets/images/Home.png',
+                      width: 25,
+                      height: 25,
+                      color: _selectedIndex == 0 ? Colors.black : Colors.grey,
+                    )),
+                label: 'Home',
+              ),
+              BottomNavigationBarItem(
+                icon: Container(
+                    margin: const EdgeInsets.only(bottom: 10),
+                    child:Image.asset('assets/images/search.png',
+                        color: _selectedIndex == 1 ? Colors.black : Colors.grey,
+                        width: 25,
+                        height: 25)),
+                label: 'Articles',
+              ),
+              BottomNavigationBarItem(
+                icon: Container(
+                    margin: const EdgeInsets.only(bottom: 10),
+                    child: Image.asset('assets/images/Profile.png',
+                        color: _selectedIndex == 2 ? Colors.black : Colors.grey,
+                        width: 25,
+                        height: 25)),
+                label: 'Profile',
+              ),
+            ],
+            type: BottomNavigationBarType.fixed),
       ),
-    );
-  }
-
-  void navigationTapped(int page) {
-    _pageController?.jumpToPage(page);
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _pageController = PageController();
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    _pageController?.dispose();
-  }
-
-  void onPageChanged(int page) {
-    setState(() {
-      _page = page;
-    });
-  }
-
-  Widget barIcon(
-      {IconData icon = Icons.home, String? text, int page = 0, bool badge = false}) {
-    return Column(
-      children: [
-        IconButton(
-          icon:  Icon(icon, size: 24.0),
-          color:
-              _page == page ? Theme.of(context).accentColor : Colors.blueGrey[300],
-          onPressed: () => _pageController?.jumpToPage(page),
-        ),
-        CustomText(text: text)
-      ],
     );
   }
 }
