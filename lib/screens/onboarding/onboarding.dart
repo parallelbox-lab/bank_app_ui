2645277b0badb56c7e7d28c4370289a44eb5d0e7
Owoeye precious <72979479+parallelbox-lab@users.noreply.github.com>
2022-07-16 22:52:08 +0100

    commit

--- lib/screens/onboarding/onboarding.dart
+++ lib/screens/onboarding/onboarding.dart
@@ -1,4 +1,6 @@
+import 'package:bank_app_ui/widgets/custom_text.dart';
 import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
 
 class Onboarding extends StatelessWidget {
   const Onboarding({ Key? key }) : super(key: key);
@@ -7,20 +9,23 @@ class Onboarding extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body:Padding(
-        padding: const EdgeInsets.all(11.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-        const  SizedBox(height: 30,),
-          Image.asset("assets/logo.png",width:180),
-       Expanded(
-         child: Center(
-          child: Image.asset("assets/let-swap.jpg"),
+      body:SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(11.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+          // const  SizedBox(height: 20,),
+            Image.asset("assets/logo.png",width:180),
+         Expanded(
+           child: Center(
+            child: Image.asset("assets/let-swap.jpg"),
+           ),
          ),
-       )
-            
-          ],
+         CustomText(text: "Easy way to manage\nyour money",size:27.sp,weight:FontWeight.bold)
+              
+            ],
+          ),
         ),
       )
     );
