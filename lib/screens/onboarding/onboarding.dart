bdad918b8c410cb40b3b7451f244d1e23e887056
Owoeye precious <72979479+parallelbox-lab@users.noreply.github.com>
2022-07-16 23:04:46 +0100

    commit

--- lib/screens/onboarding/onboarding.dart
+++ lib/screens/onboarding/onboarding.dart
@@ -8,23 +8,28 @@ class Onboarding extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      // backgroundColor: Colors.white,
       body:SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(11.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-          // const  SizedBox(height: 20,),
+          const  SizedBox(height: 20,),
             Image.asset("assets/logo.png",width:180),
          Expanded(
            child: Center(
             child: Image.asset("assets/let-swap.jpg"),
            ),
          ),
-         CustomText(text: "Easy way\nto manage your money",size:33.sp,weight:FontWeight.bold),
+         Expanded(child: Column(
+           children: [
+             CustomText(text: "Easy way\nto manage your money",size:33.sp,weight:FontWeight.bold),
+             const SizedBox(height:10),
+             CustomText(text: "Connect your money to your friemds and brands",size:15.sp,color:Colors.grey,weight:FontWeight.w700),
+           ],
+         )),
          const SizedBox(height:10),
-         CustomText(text: "Connect your money to your friemds and brands",size:15.sp,color:Colors.grey,weight:FontWeight.w700),
          Container(
           width: double.infinity,
           alignment: Alignment.bottomLeft,
